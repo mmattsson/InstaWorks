d11ba9dfe48cba9c9b974919a43caadb0c74e9ce
Mattias Mattsson <mm_github@mattsson.info>
2014-06-24 19:14:31 -0700

    Fixed incorrect ignoring of self-test script.

---
+++ bin/run_selftest.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+command -v valgrind > /dev/null
+HAS_VALGRIND=$?
+
+if [ $HAS_VALGRIND = "0" ]; then
+    valgrind -q --error-exitcode=123 --leak-check=full "$DIR/selftest"
+    _EXIT_CODE=$?
+    echo ""
+    if [ "$_EXIT_CODE" = "123" ]; then
+        echo " THERE WERE MEMORY ERRORS PRESENT!"
+    else 
+        echo " No memory errors present."
+    fi
+    echo ""
+else
+    "$DIR/selftest"
+fi
+
