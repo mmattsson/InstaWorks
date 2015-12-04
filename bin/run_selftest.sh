73e688ed1f595c0051928490e405266e18b150c4
Mattias Mattsson <mm_github@mattsson.info>
2015-12-03 20:14:59 -0800

    Moved selftest compiled binary to selftest directory.

--- bin/run_selftest.sh
+++ bin/run_selftest.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TEST_BIN=$DIR/../selftest/selftest
 
 # Check if valgrind is present
 ! command -v valgrind > /dev/null
@@ -8,12 +9,12 @@ HAS_VALGRIND=$?
 
 # If we're just querying the selftest binary, then don't run the test
 if [ "$#" = "0" -o "$1" = "show" ]; then
-    $DIR/selftest $*
+    $TEST_BIN $*
     exit 0
 fi
 
 if [ $HAS_VALGRIND = "1" ]; then
-    valgrind -q --error-exitcode=123 --leak-check=full "$DIR/selftest" $*
+    valgrind -q --error-exitcode=123 --leak-check=full "$TEST_BIN" $*
     _EXIT_CODE=$?
     echo ""
     if [ "$_EXIT_CODE" = "123" ]; then
@@ -23,6 +24,6 @@ if [ $HAS_VALGRIND = "1" ]; then
     fi
     echo ""
 else
-    $DIR/selftest $*
+    $TEST_BIN $*
 fi
 
